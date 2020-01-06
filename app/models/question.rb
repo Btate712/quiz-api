75c32a337497fd3595007e4a0fdb8a1cdee36266
Robert Tate <roberttate@Roberts-MacBook-Pro.local>
2020-01-06 11:16:09 -0600

    Built active record associations and added db seed data

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,6 @@
 class Question < ApplicationRecord
+  belongs_to :topic
+
+  has_many :encounters
+  has_many :comments 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
+
+  has_many :encounters
+  has_many :comments
+  has_many :user_topics
+
+  has_many :topics, through: :user_topics
 end
