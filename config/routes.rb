0665c0486d219784955bc68af1b4562f15b73ebf
Robert Tate <roberttate@Roberts-MacBook-Pro.local>
2020-01-06 15:30:18 -0600

    added resources for encounters to config/routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   post 'auth/login', to: 'users#login'
   get 'test', to: 'users#test'
 
-  resources :topics, :questions 
+  resources :topics, :questions
+  resources :encounters, only: [:create, :show]
 end
