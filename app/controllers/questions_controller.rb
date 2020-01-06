bf2f1b7b97dfd7f90c24be5e6b9da721b8b6dada
Robert Tate <roberttate@Roberts-MacBook-Pro.local>
2020-01-06 14:26:23 -0600

    added question show route

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,5 +4,23 @@ class QuestionsController < ApplicationController
     questions = []
     topics.each { |topic| questions.concat(topic.questions) }
     render json: { status: "success", body: questions, message: "#{questions.length} questions found" }
-  end 
+  end
+
+  def show
+    question = Question.find(params[:id])
+    topic = question.topic
+    if topic
+      if @current_user.has_topic_read_rights(topic)
+        render json: {
+          status: "success",
+          body: { question: question },
+          message: "question ##{question.id} received"
+        }
+      else
+        render json: { status: "fail", message: "user does not have read access to this question"}
+      end
+    else
+      render json: { status: "fail", message: "question/topic not found" }
+    end
+  end
 end
