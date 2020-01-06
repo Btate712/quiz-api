24c210aab14d89a49afc6cea9de53e180edd06d6
Robert Tate <roberttate@Roberts-MacBook-Pro.local>
2020-01-06 15:28:38 -0600

    Built encounter model "stats" method and encounter controller create and show methods

---
+++ app/controllers/encounters_controller.rb
@@ -0,0 +1,23 @@
+class EncountersController < ApplicationController
+  def create
+    failed = 0
+    user_id = @current_user
+    params[:questions].each do |question|
+      e = Encounter.new(user_id: user_id, question_id: question[:id],
+        selected_answer: question[:choice])
+      if @current_user.has_topic_read_rights(e.question.topic)
+        failed += 1 if !e.save
+      end
+    end
+    render json: { message: "#{failed} encounters failed to save." }
+  end
+
+  def show
+    # show method is actually being used as an index method filtered to
+    # only provide information for the user whose user_id is supplied via the
+    # ":id" portion of the URL.  The information returned via json is a summary
+    # of quiz question performance for each topic.
+    user_stats = Encounter.stats(@current_user)
+    render json: { message: user_stats }
+  end
+end
