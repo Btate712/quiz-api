0c6e0a6776c2f6007a103e0d816c711eb8b2d7dd
btate712 <btate712@gmail.com>
2020-01-29 12:44:30 -0600

    Fixed bug - number_of_questions changed from string to int

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,7 +1,7 @@
 class QuizzesController < ApplicationController
   def create
     topic_ids = params[:topicIds].split(",")
-    number_of_questions = params[:numberOfQuestions]
+    number_of_questions = params[:numberOfQuestions].to_i
     
     render json: { quiz: Quiz.makeQuiz(number_of_questions, topic_ids) }
     
