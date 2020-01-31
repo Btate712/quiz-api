35482a7a0569941a73d8d51de872d4a93de7bd2b
btate712 <btate712@gmail.com>
2020-01-31 12:36:04 -0600

    troubleshooting cors issues

--- config/application.rb
+++ config/application.rb
@@ -26,8 +26,9 @@ module QuizApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', 
-        :headers: :any, :methods: [:get, :post, :patch, :put, :delete, :options]
+        resource ('*', 
+        :headers: :any, 
+        :methods: [:get, :post, :patch, :put, :delete, :options])
       end
     end
 
