37ada2d3eb3b7b51a19f462e7f34cb4db96efead
btate712 <btate712@gmail.com>
2020-02-10 18:39:49 -0600

    added "type" column to comment table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_09_214515) do
+ActiveRecord::Schema.define(version: 2020_02_11_003830) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_01_09_214515) do
     t.boolean "resolved"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "type"
   end
 
   create_table "encounters", force: :cascade do |t|
