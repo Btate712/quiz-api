d658e84ba75c3b6cf492f5b6b4a36f2dc8170551
btate712 <btate712@gmail.com>
2020-02-15 09:27:30 -0600

    added validation to UserTopic to ensure user is not assigned to a topic multiple times

--- app/models/project_topic.rb
+++ app/models/project_topic.rb
@@ -3,4 +3,5 @@ class ProjectTopic < ApplicationRecord
   belongs_to :topic
 
   validates_presence_of :topic_id, :project_id
+  validates_uniqueness_of :project_id, scope: :topic_id, message: "Topic is already a member of this project."
 end
