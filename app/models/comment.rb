69ac13fb33793afaea0ebf5a6e52993104ae80fd
btate712 <btate712@gmail.com>
2020-02-19 12:07:03 -0600

    removed activerecord validation for comment text and added validation for comment_type

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :question
 
-  validates_presence_of :user_id, :question_id, :text
+  validates_presence_of :user_id, :question_id, :comment_type
 end
