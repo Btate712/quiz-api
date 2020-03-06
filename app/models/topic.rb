09b902875a6c22998de55d2f015a6029a31c1d78
btate712 <btate712@gmail.com>
2020-03-06 09:38:27 -0600

    added date last question added to topics index

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -7,11 +7,15 @@ class Topic < ApplicationRecord
   validates_presence_of :name
   
   def self.for_user_as_hash(user)
-    self.for_user(user).map { |topic| { topic: topic, questionCount: topic.questions.count }}
+    self.for_user(user).map { |topic| { topic: topic, questionCount: topic.questions.count, dateLastQuestionAdded: topic.date_last_question_added }}
   end
 
   def self.for_user(user)
     user.is_admin? ? Topic.all : Topic.all.filter { |topic| user.has_topic_rights?(topic, 10) }
+  end
+
+  def date_last_question_added
+    self.questions.max_by{ |question| question.created_at }.created_at
   end
 
   def destroy_dependents
