b60fc6acd6918fbed7fa602249357910bb275bd6
btate712 <btate712@gmail.com>
2021-10-21 18:37:36 -0500

    Test

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@ Rails.application.routes.draw do
 
   get '/', to: 'users#root'
 
-  get '/', to: 'questions#test'
-  
+  get 'questions/test', to: 'questions#test'
+
   post 'questions/batch', to: 'questions#batch_create'
 
   get '/users/me', to: 'users#get_user'
